d5955541ce5e2dd8a9d10ea086bf6d89697a55a3
nikolaDrljaca <devnikoladr@gmail.com>
2023-04-18 19:30:34 +0300

    Initial commit.

---
+++ .wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where we can actually apply our configs
+
+config.color_scheme = "Gruvbox dark, medium (base16)"
+
+config.font = wezterm.font("RecMonoDuotone Nerd Font")
+config.font_size = 13.0
+config.initial_cols = 120
+config.initial_rows = 30
+
+return config
