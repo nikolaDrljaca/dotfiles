66229c752c10bf6259110693159aeaaab8bfd572
nikolaDrljaca <devnikoladr@gmail.com>
2023-04-20 21:16:30 +0300

    Added NoNeckPain plugin to nvim

--- NvimConfig/custom/chadrc.lua
+++ NvimConfig/custom/chadrc.lua
@@ -1,5 +1,5 @@
 local M = {}
- M.ui = {theme = 'gruvbox'}
+M.ui = { theme = "gruvbox" }
 M.plugins = "custom.plugins"
-M.mappings = require "custom.mappings"
- return M
+M.mappings = require("custom.mappings")
+return M
--- NvimConfig/custom/mappings.lua
+++ NvimConfig/custom/mappings.lua
@@ -1,22 +1,23 @@
 local M = {}
 
 M.custom = {
-  n = {
-    ["<C-d>"] = { "<C-d>zz" },
-    ["<C-u>"] = { "<C-u>zz" },
-    ["<A-j>"] = { ":m .+1<CR>==" },
-    ["<A-k>"] = { ":m .-2<CR>==" },
-    ["<leader>todo"] = { ":TodoQuickFix <CR>", "List tags from todo-comments" },
-  },
-  v = {
-    ["<C-d>"] = { "<C-d>zz" },
-    ["<C-u>"] = { "<C-u>zz" },
-    ["<A-j>"] = { ":m '>+1<CR>gv=gv" },
-    ["<A-k>"] = { ":m '<-2<CR>gv=gv" },
-  },
-  i = {
-    ["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
-  },
+	n = {
+		["<C-d>"] = { "<C-d>zz" },
+		["<C-u>"] = { "<C-u>zz" },
+		["<A-j>"] = { ":m .+1<CR>==" },
+		["<A-k>"] = { ":m .-2<CR>==" },
+		["<leader>todo"] = { ":TodoQuickFix <CR>", "List tags from todo-comments" },
+		["<leader>z"] = { ":NoNeckPain <CR>", "Center on screen buffer" },
+	},
+	v = {
+		["<C-d>"] = { "<C-d>zz" },
+		["<C-u>"] = { "<C-u>zz" },
+		["<A-j>"] = { ":m '>+1<CR>gv=gv" },
+		["<A-k>"] = { ":m '<-2<CR>gv=gv" },
+	},
+	i = {
+		["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
+	},
 }
 
 return M
