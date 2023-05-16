7469d3c37e07e485e58def327eaa56c115dee728
nikolaDrljaca <devnikoladr@gmail.com>
2023-05-16 21:10:06 +0300

    Chagned font

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,14 +3,14 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- This is where we can actually apply our configs
 
 config.color_scheme = "Gruvbox dark, medium (base16)"
 
-config.font = wezterm.font("CaskaydiaCove Nerd Font")
+config.font = wezterm.font("Hurmit Nerd Font")
 config.font_size = 13.0
 config.initial_cols = 120
 config.initial_rows = 30
