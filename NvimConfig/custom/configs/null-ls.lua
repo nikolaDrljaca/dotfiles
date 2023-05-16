0f883a8ceae133f0acce978331bf39d30140c34c
nikolaDrljaca <devnikoladr@gmail.com>
2023-05-16 21:12:18 +0300

    nvim config update

--- NvimConfig/custom/configs/null-ls.lua
+++ NvimConfig/custom/configs/null-ls.lua
@@ -6,27 +6,28 @@ local lint = null_ls.builtins.diagnostics
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local sources = {
-	formatting.prettierd.with({
-		extra_filetypes = { "svelte" },
-	}),
-	formatting.stylua,
-	null_ls.builtins.formatting.autopep8,
+  formatting.prettierd.with({
+    extra_filetypes = { "svelte" },
+  }),
+  formatting.stylua,
+  null_ls.builtins.formatting.autopep8,
+  null_ls.builtins.diagnostics.php,
 }
 
 null_ls.setup({
-	debug = true,
-	sources = sources,
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
+  debug = true,
+  sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
