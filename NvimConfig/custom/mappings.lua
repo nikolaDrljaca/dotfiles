318dc3c9b336801d23ebc6fe26bd1537e8383dfa
nikolaDrljaca <devnikoladr@gmail.com>
2023-05-24 14:23:48 +0300

    Mappings change for jump end/start of line.

--- NvimConfig/custom/mappings.lua
+++ NvimConfig/custom/mappings.lua
@@ -1,24 +1,28 @@
 local M = {}
 
 M.custom = {
-	n = {
-		["<C-d>"] = { "<C-d>zz" },
-		["<C-u>"] = { "<C-u>zz" },
-		["<C-l>"] = { "<S-v>yp" },
-		["<A-j>"] = { ":m .+1<CR>==" },
-		["<A-k>"] = { ":m .-2<CR>==" },
-		["<leader>todo"] = { ":TodoQuickFix <CR>", "List tags from todo-comments" },
-		["<leader>z"] = { ":NoNeckPain <CR>", "Center on screen buffer" },
-	},
-	v = {
-		["<C-d>"] = { "<C-d>zz" },
-		["<C-u>"] = { "<C-u>zz" },
-		["<A-j>"] = { ":m '>+1<CR>gv=gv" },
-		["<A-k>"] = { ":m '<-2<CR>gv=gv" },
-	},
-	i = {
-		["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
-	},
+  n = {
+    ["<C-d>"] = { "<C-d>zz" },
+    ["<C-u>"] = { "<C-u>zz" },
+    ["<C-l>"] = { "<S-v>yp" },
+    ["<A-j>"] = { ":m .+1<CR>==" },
+    ["<A-k>"] = { ":m .-2<CR>==" },
+    ["<leader>todo"] = { ":TodoQuickFix <CR>", "List tags from todo-comments" },
+    ["<leader>z"] = { ":NoNeckPain <CR>", "Center on screen buffer" },
+    ["<S-e>"] = { "$", "Jump to end of line." },
+    ["<S-b>"] = { "_", "Jump to front of line." },
+  },
+  v = {
+    ["<C-d>"] = { "<C-d>zz" },
+    ["<C-u>"] = { "<C-u>zz" },
+    ["<A-j>"] = { ":m '>+1<CR>gv=gv" },
+    ["<A-k>"] = { ":m '<-2<CR>gv=gv" },
+    ["<S-e>"] = { "$", "Jump to end of line." },
+    ["<S-b>"] = { "_", "Jump to front of line." },
+  },
+  i = {
+    ["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
+  },
 }
 
 return M
