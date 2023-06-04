bbc4b86264469f94f24928152ba3f1fa6d1fcc23
nikolaDrljaca <devnikoladr@gmail.com>
2023-06-04 14:48:20 +0300

    config updates and tailwind formatting

--- NvimConfig/custom/configs/null-ls.lua
+++ NvimConfig/custom/configs/null-ls.lua
@@ -1,33 +1,33 @@
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-local sources = {
-  formatting.prettierd.with({
-    extra_filetypes = { "svelte" },
-  }),
-  formatting.stylua,
-  null_ls.builtins.formatting.autopep8,
-  null_ls.builtins.diagnostics.php,
+local opts = {
+	sources = {
+		formatting.prettierd.with({
+			extra_filetypes = { "svelte" },
+		}),
+		formatting.stylua,
+		formatting.autopep8,
+		formatting.rustywind,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({
+				group = augroup,
+				buffer = bufnr,
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 }
 
-null_ls.setup({
-  debug = true,
-  sources = sources,
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
-})
+return opts
--- NvimConfig/custom/plugins.lua
+++ NvimConfig/custom/plugins.lua
@@ -1,77 +1,75 @@
 local plugins = {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        --defaults
-        "vim",
-        "lua",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				--defaults
+				"vim",
+				"lua",
 
-        -- web dev
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-        "json",
-        "svelte",
-        "php",
+				-- web dev
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
+				"svelte",
+				"php",
 
-        -- JVM
-        "java",
-        "kotlin",
+				-- JVM
+				"java",
+				"kotlin",
 
-        -- Misc
-        "markdown",
-        "dockerfile",
-        "sql",
-      },
-      indent = { enabled = true },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require("custom.configs.null-ls")
-      end,
-    },
-
-    config = function()
-      require("plugins.configs.lspconfig")
-      require("custom.configs.lspconfig")
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "tailwindcss-language-server",
-        "pyright",
-        "typescript-language-server",
-        "emmet-ls",
-        "eslint-lsp",
-        "prettierd",
-        "stylua",
-        "autopep8",
-        "svelte-language-server",
-        "phpactor",
-      },
-    },
-  },
-  {
-    "folke/todo-comments.nvim",
-    config = function()
-      require("custom.configs.todo-comments")
-    end,
-    lazy = false,
-  },
-  {
-    "shortcuts/no-neck-pain.nvim",
-    version = "*",
-    lazy = false,
-  },
+				-- Misc
+				"markdown",
+				"dockerfile",
+				"sql",
+			},
+			indent = { enabled = true },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"tailwindcss-language-server",
+				"pyright",
+				"typescript-language-server",
+				"emmet-ls",
+				"eslint-lsp",
+				"prettierd",
+				"stylua",
+				"autopep8",
+				"svelte-language-server",
+				"rustywind",
+			},
+		},
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		opts = function()
+			return require("custom.configs.null-ls")
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		config = function()
+			require("custom.configs.todo-comments")
+		end,
+		lazy = false,
+	},
+	{
+		"shortcuts/no-neck-pain.nvim",
+		version = "*",
+		lazy = false,
+	},
 }
 return plugins
