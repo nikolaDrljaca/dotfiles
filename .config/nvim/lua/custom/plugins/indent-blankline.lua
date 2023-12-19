e48a1054dc271e04439bf028f079780a32a4f148
nikolaDrljaca <devnikoladr@gmail.com>
2023-12-19 13:10:04 +0100

    configure indent blankline

--- .config/nvim/lua/custom/plugins/indent-blankline.lua
+++ .config/nvim/lua/custom/plugins/indent-blankline.lua
@@ -2,7 +2,12 @@ local ibl = {
   'lukas-reineke/indent-blankline.nvim',
   main = 'ibl',
   opts = {
-    indent = { char = "│"}
+    -- indent = { char = "│"},
+    scope = {
+      show_start = false,
+      show_end = false,
+      show_exact_scope = false
+    }
   },
 }
 
