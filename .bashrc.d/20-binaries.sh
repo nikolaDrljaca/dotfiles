cd8dc8e199555179d7037e87d182a5dce6c82d07
nikolaDrljaca <devnikoladr@gmail.com>
2026-05-24 12:24:53 +0200

    (shell): introduce bash dir

---
+++ .bashrc.d/20-binaries.sh
@@ -0,0 +1,6 @@
+# Local binaries
+export PATH="$PATH:/home/nikola/.local/bin"
+# Local scripts
+export PATH="$PATH:$HOME/.local/scripts"
+# JetBrains Toolbox
+export PATH="$PATH:/home/nikola/.local/share/JetBrains/Toolbox/apps"
---
+++ .bashrc.d/31-propmt.sh
@@ -0,0 +1,2 @@
+eval "$(oh-my-posh init bash --config /home/nikola/.poshthemes/huvix-2.omp.json)"
+eval "$(thefuck --alias)"
